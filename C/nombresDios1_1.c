b71e4e517c18a9407bc6197fd92868d45ea0d2cc
jm2garrido <jm2garrido@users.noreply.github.com>
2017-07-20 01:20:19 +0200

    Different directories for different languages
    Some changes in python 3 version

---
+++ C/nombresDios1_1.c
@@ -0,0 +1,135 @@
+/*
+ *      nombresDios1_0.c
+ *      
+ *      Copyright 2009 josemi <josemi@darksun>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+/* linux gcc nombresDios1_0.c -march=x86-64 -o nombresDios */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv)
+{
+#define CAR  (13)
+#define POS  (9)
+#define MAXBLOCK  (3)
+#define TRUE (1)
+#define FALSE (0)
+/* esto es la edad media */		
+		
+		long long sum = 0;
+		long long sumOK = 0;
+		/* supongo long long 64 bits */
+		
+		
+		int ended = FALSE;
+	
+		int l,k,j,e;
+		/* vaya mierda, tengo que declarar esto aqui */
+
+		int car = CAR;
+		int pos = POS;
+
+		if(argc == 2)
+		{
+			car = atoi(argv[1]);
+			pos = atoi(argv[2]);
+		}
+	
+		int *cell = calloc(pos,sizeof(int));	
+	
+		
+		printf("Version %ld, CAR %d, POS %d, MAXBLOCK %d\n",
+				sizeof(int)*8,car,pos,MAXBLOCK);
+
+		clock_t begin = clock();
+		
+		do {	
+			sum++;
+			
+			int numOK = TRUE;
+			for(k=0; (numOK) && (k<pos-MAXBLOCK);k++)
+			{
+				numOK = FALSE;
+				for(e = k + 1; (!numOK) && (e < k+MAXBLOCK+1); e++)
+				{
+					
+					if(cell[k]!=cell[e])
+					{
+						
+						numOK = TRUE;
+					}
+				}
+				
+			}
+				
+			
+			if(numOK)
+			{
+				sumOK++;
+				/*
+				StringBuffer a= new StringBuffer();
+				for(int j=POS-1;j>=0;j--)
+				{
+					a.append((char)(65+cell[j]));
+				}
+				System.out.println(a);
+				*/
+				/*
+				for(j=POS-1;j>=0;j--)
+				{
+					printf("%c",65+cell[j]);
+				}
+				printf("\n");
+				*/
+			}
+			
+			
+			cell[0]++;
+			int i=0;
+			while(cell[i]>=car)
+			{
+				cell[i]=0;
+				i++;
+				if(i<pos)
+				{
+					cell[i]++;
+				}
+				else
+				{
+					ended = TRUE;
+					break;
+				}
+			}
+
+		} while(!ended);
+		
+		free(cell);	
+		
+		clock_t end = clock();
+		
+		printf("Sum   %lld \n",sum);
+		
+		printf("SumOK %lld \n",sumOK);
+		
+		printf("Time: %ld \n",((end-begin)*1000)/CLOCKS_PER_SEC);
+
+	return 0;
+}
---
+++ python/nombresDios_3_1.2.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+"""
+NamesOfGod
+
+Version para Python 3
+"""
+
+__author__ = "Jose Miguel Garrido"
+__copyright__ = "(C) 2016 Jose Miguel Garrido"
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import time
+import numpy as np
+import sys
+
+def nombresDios(car,pos,maxblock):
+    #estos numeros pueden ser grandes, mas de 32 bits
+    # hay que usar los numeros de precision arbitraria
+    sum_ = 0
+    sumOK = 0
+
+    # matriz eficiente de enteros
+    cell = np.zeros(pos,"int64")
+
+    ended = False
+    while not ended:
+        sum_+=1
+
+        numOK = True
+
+        #bucle chapucero para emular el for de java
+        k=0
+        while ((numOK) and (k<pos-maxblock)):
+            numOK=False
+            e = k + 1
+            while ((not numOK) and (e < k+maxblock+1)):
+                #print("k: {0} e: {1}".format(k,e))
+                if (cell[k]!=cell[e]):
+                    #print("Verdad")
+                    numOK =True
+                e+=1
+            k+=1
+
+        if(numOK):
+            sumOK+=1
+            # print(cell)
+
+        cell[0]+=1
+
+        i=0    
+        while(cell[i]>=car):
+            cell[i]=0
+            i+=1
+            if i<pos:
+                cell[i]+=1
+            else:
+                ended= True
+                break
+            
+    return sum_,sumOK
+# end function
+
+if __name__ == '__main__':
+    #las constantes
+    CAR = 13
+    POS = 7
+    MAXBLOCK = 3
+
+    if len(sys.argv) == 4:
+        car = int(sys.argv[1])
+        pos = int(sys.argv[2])
+        maxblock = int(sys.argv[3])
+    else:
+        car = CAR
+        pos = POS
+        maxblock = MAXBLOCK
+
+    print("Caracteres:   {}".format(car))
+    print("Posiciones:   {}".format(pos))
+    print("Repeticiones: {}".format(maxblock))
+
+
+    # aqui podria ser clock. pero eso mide el processor time en muchos sistemas
+    # por coherencia con otras versiones, queremos medir el wall-time
+    # ojo, la version en C usa clock
+    begin = time.time()
+
+    sum_, sumOK = nombresDios(car,pos,maxblock)
+
+    #acabados los calculos, pongo los resultados
+
+    end = time.time()
+
+    print("Sum   {0}".format(sum_))
+    print("SumOK {0}".format(sumOK))
+    print("Time: {0}".format((end-begin)*1000))
+            
+
